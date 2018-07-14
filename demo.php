005ec279ba5adccca7593c94a0e6b83c9860217e
gycp1234 <457172268@qq.com>
2018-07-14 09:45:17 +0800

    第一次测试

---
+++ demo.php
@@ -0,0 +1,13 @@
+<?php 
+//d导入composer自带的加载文件
+require './vendor/autoload.php';
+//使用
+$data=[
+   ['name'=>'笑话','age'=>18],
+   ['name'=>'都是','age'=>19],
+   ['name'=>'钜惠','age'=>17],
+   ['name'=>'天然','age'=>13]
+];
+var_dump($data);
+dd($data);
+?>
